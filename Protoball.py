055d5bd6dd30817ac5add4e83ba3dd13b7f8031c
PinkPantser <112284600+PinkPantser@users.noreply.github.com>
2023-03-24 18:30:43 -0400

    Create Protoball.py

---
+++ Protoball.py
@@ -0,0 +1,122 @@
+#Object Tracking
+
+import cv2
+import numpy as np
+#import serial
+import time
+# Initalize camera
+cap = cv2.VideoCapture(0)
+
+#Initialize serial communication
+#ser = serial.Serial('/dev/ttyACM0',9600)
+
+print('writing to arduino serial port')
+
+# define range of purple color in RGB
+lower_purple = np.array([200,200,0])
+upper_purple = np.array([255,255,100])
+
+# Create empty points array
+points = []
+
+# Get default camera window size
+ret, frame = cap.read()
+time.sleep(3)
+Height, Width = frame.shape[:2]
+frame_count = 0
+frame_send = 0
+trigger = 0
+time.sleep(5)
+
+while True:
+	
+    
+    # Capture webcame frame
+    ret, frame = cap.read()
+    hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv_img, lower_purple, upper_purple)
+
+     # Perform Bitwise AND on mask and our original frame
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # Find contours, OpenCV 3.X users use this line instead
+    _, contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Create empty centre array to store centroid center of mass
+    center =   int(Height/2), int(Width/2)
+    radius = 0
+
+    if len(contours) > 0:
+        
+        # Get the largest contour and its center 
+        c = max(contours, key=cv2.contourArea)
+        (x, y), radius = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        try:
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        except:
+            center =   int(Height/2), int(Width/2)
+
+        # Allow only countors that have a larger than 15 pixel radius
+        if radius > 25:
+            
+            # Draw cirlce and leave the last center creating a trail
+            cv2.circle(frame, (int(x), int(y)), int(radius),(0, 0, 255), 2)
+            cv2.circle(frame, center, 5, (0, 255, 0), -1)
+           
+    # Log center points 
+    points.append(center)
+    
+    # loop over the set of tracked points
+    if radius > 25:
+        for i in range(1, len(points)):
+            try:
+                cv2.line(frame, points[i - 1], points[i], (0, 255, 0), 2)
+            except:
+                pass
+            
+        # Make frame count,zero
+        frame_count = 0
+        frame_send += 1
+        trigger = 1
+        if int(x) <= 200:
+          trigger = 0    
+        if int(x) >= 400:
+          trigger = 2
+
+        if frame_send >= 100:
+           # if trigger == 1:
+           #  ser.write ('1')
+            #if trigger == 2:
+           #  ser.write ('2')
+           # if trigger == 0:
+			# ser.write ('0')
+          print (radius)
+          print(trigger)
+          frame_send = 0
+    
+    else:
+        # Count frames 
+        frame_count += 1
+        
+        # If we count 10 frames without object lets delete our trail
+        if frame_count == 10:
+            points = []
+            # when frame_count reaches 10 let's clear our trail 
+            frame_count = 0
+            
+    # Display our object tracker
+    frame = cv2.flip(frame, 1)
+    cv2.imshow("Object Tracker", frame)
+   
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+
+# Release camera and close any open windows
+cap.release()
+cv2.destroyAllWindows()
+
